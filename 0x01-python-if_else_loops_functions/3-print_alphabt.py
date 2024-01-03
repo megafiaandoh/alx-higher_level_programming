ea113b72f04259e9156ca6f337cdeab79fd0463e
Margaret Andoh <andoh26@gmail.com>
2024-01-02 16:00:50 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
+for i in range(97, 123):
+    if i in [101, 113]:
+        continue
+    print("{}".format(chr(i)), end='')
 
