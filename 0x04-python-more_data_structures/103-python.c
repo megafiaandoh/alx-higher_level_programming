7f26443021642e4139b34ca26fcaf519d075e25e
Margaret Andoh <andoh26@gmail.com>
2024-01-10 13:59:26 -0800

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -6,7 +6,7 @@
 void print_python_bytes(PyObject *p)
 {
 	long int size;
-	int a;
+	int i;
 	char *trying_str = NULL;
 
 	printf("[.] bytes object info\n");
@@ -24,26 +24,26 @@ void print_python_bytes(PyObject *p)
 		printf("  first %li bytes:", size + 1);
 	else
 		printf("  first 10 bytes:");
-	for (a = 0; a <= size && a < 10; a++)
-		printf(" %02hhx", trying_str[a]);
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02hhx", trying_str[i]);
 	printf("\n");
 }
 
 void print_python_list(PyObject *p)
 {
         long int size = PyList_Size(p);
-        int a;
+        int i;
         PyListObject *list = (PyListObject *)p;
         const char *type;
 
         printf("[*] Python list info\n");
         printf("[*] Size of the Python List = %li\n", size);
         printf("[*] Allocated = %li\n", list->allocated);
-        for (a = 0; a < size; a++)
+        for (i = 0; i < size; i++)
         {
-                type = (list->ob_item[a])->ob_type->tp_name;
-		printf("Element %a: %s\n", a, type);
+                type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
                 if (!strcmp(type, "bytes"))
-                        print_python_bytes(list->ob_item[a]);
+                        print_python_bytes(list->ob_item[i]);
         }
 }
