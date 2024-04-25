742e806833c2549417c03f43d9a1a6ea03fdea2d
megafiaandoh <andoh26@gmail.com>
2024-04-25 08:39:22 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl "$1" -sX GET -H "X-HolbertonSchool-User-Id:98"
+# shell script to send a get request and create acustomer header
+curl -sH "X-School-User-Id: 98" "$1"
